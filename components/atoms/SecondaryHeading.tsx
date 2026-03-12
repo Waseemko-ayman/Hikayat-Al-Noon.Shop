bbb7f89bbd808c81dbbb5d9c01cff65d6c7c0bb8
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2026-03-12 13:35:09 +0200

    fix(SecondaryHeading): update heading level to h3 for proper accessibility order

--- components/atoms/SecondaryHeading.tsx
+++ components/atoms/SecondaryHeading.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 const SecondaryHeading = ({ title }: { title: string }) => {
   const StyledHeading = 'text-[17px] font-bold mb-3.5';
   return (
-    <motion.h4
+    <motion.h3
       initial={{ opacity: 0, y: 60 }}
       whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true, amount: 0.2 }}
@@ -13,7 +13,7 @@ const SecondaryHeading = ({ title }: { title: string }) => {
       className={StyledHeading}
     >
       {title}
-    </motion.h4>
+    </motion.h3>
   );
 };
 
