9812accaa5f388db5ba8071197c7a3e3534805f5
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2026-03-20 08:36:44 +0200

    style: simplify card background and center image
    - Replaced custom CSS variable with static white background
    - Centered image using mx-auto for better alignment
    - Minor UI cleanup for more consistent card appearance

--- components/ui/focus-cards.tsx
+++ components/ui/focus-cards.tsx
@@ -14,14 +14,14 @@ export const Card = React.memo(
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.4, delay: index * 0.1 }}
         viewport={{ once: true, amount: 0.2 }}
-        className="bg-(--white-color) max-w-full border border-[#cce7d0] py-6 px-4 shadow-[20px_20px_34px_rgba(0,0,0,0.03)] text-center hover:shadow-[10px_10px_54px_rgba(70,62,221,0.1)] hover:-translate-y-2 transition-all duration-200"
+        className="bg-white max-w-full border border-[#cce7d0] py-6 px-4 shadow-[20px_20px_34px_rgba(0,0,0,0.03)] text-center hover:shadow-[10px_10px_54px_rgba(70,62,221,0.1)] hover:-translate-y-2 transition-all duration-200"
       >
         <Image
           src={card.image}
           alt="features"
           width={200}
           height={200}
-          className="mb-3 md:w-full"
+          className="mb-3 md:w-full mx-auto"
         />
         <span
           className={`block text-sm md:text-[12px] text-(--forth-color) p-2 rounded-sm font-semibold ${card.bgColor}`}
