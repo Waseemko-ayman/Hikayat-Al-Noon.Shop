4a9aa13c53b1057fc8514dcfdea4ac0a10ea803e
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2026-03-23 21:59:58 +0200

    (FeatursSec): Delete padding top zero

--- components/organism/FeatursSec.tsx
+++ components/organism/FeatursSec.tsx
@@ -4,7 +4,7 @@ import ResponsiveWrapper from '../molecules/ResponsiveWrapper';
 
 const FeatursSec = () => {
   return (
-    <ResponsiveWrapper otherClassName="pt-0! pb-8!">
+    <ResponsiveWrapper otherClassName="pb-8!">
       <FocusCards cards={FETURES} />
     </ResponsiveWrapper>
   );
