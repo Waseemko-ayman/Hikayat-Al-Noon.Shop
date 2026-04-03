6f2d6c98c54d17b0171e180c7f460e055bfa4a9a
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2026-04-03 16:18:26 +0300

    Pass MainHightScreen prop to Loading Clothes in ShopPage

--- features/shop/index.tsx
+++ features/shop/index.tsx
@@ -1,11 +1,10 @@
 "use client";
 import dynamic from 'next/dynamic';
-import React from 'react';
 import RepairServices from './Sections/RepairServices';
 import Loading from '@/components/atoms/Loading';
 const Clothes = dynamic(() => import('./Sections/Clothes'), {
   ssr: false,
-  loading: () => <Loading loadingText="Loading products..." />,
+  loading: () => <Loading loadingText="Loading products..." MainHightScreen />,
 });
 
 const ShopPage = () => {
