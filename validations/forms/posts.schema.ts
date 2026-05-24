938c5182b75312807927307dc0811a6c4c8a355e
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2026-05-25 02:01:40 +0300

    feat(posts): add body field and integrate dynamic categories select from DB

--- validations/forms/posts.schema.ts
+++ validations/forms/posts.schema.ts
@@ -3,6 +3,7 @@ import * as yup from 'yup';
 export const createPostsSchema = yup.object({
   title: yup.string().required('Title is required'),
   excerpt: yup.string().max(200, 'Excerpt too long').nullable(),
+  body: yup.string().required('Body is required'),
   image: yup.mixed().required('Image is required'),
   category: yup.string().required('Category is required'),
   date: yup.string().required('Date is required'),
